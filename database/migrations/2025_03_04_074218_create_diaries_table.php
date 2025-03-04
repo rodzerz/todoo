1f171ee9ff5ca42d99d42ba2d7843bb2b0aceecf
Skolnieks <skolnieks.pjvt@pjvt.local>
2025-03-04 09:46:39 +0200

    a;sldfha;dlfh

---
+++ database/migrations/2025_03_04_074218_create_diaries_table.php
@@ -0,0 +1,26 @@
+<?php
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('diaries', function (Blueprint $table) {
+            $table->id();
+            $table->string("content");
+            $table->boolean("completed");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('to_dos');
+    }
+
+
+};
